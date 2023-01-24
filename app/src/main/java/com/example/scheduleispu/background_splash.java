2c50d71f7c9f476ad81e4c65f6c96db50422ad98
dfoxs353 <platoleg37@gmail.com>
2023-01-24 23:56:21 +0300

    Delete Dark Theme

--- app/src/main/java/com/example/scheduleispu/background_splash.java
+++ app/src/main/java/com/example/scheduleispu/background_splash.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class background_splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
